cfad79737035bd0cae303f4f976d85e82af46470
satpathyDS26 <satpathyiasri@gmail.com>
2026-05-02 04:13:14 +0530

    Task 2 first commit

---
+++ Task_2_api.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request, Response
+import json
+
+app = Flask(__name__)
+
+# Load JSON data once
+with open("Task 1 - Satpathy - Parser.json", "r") as f:
+    cargo_data = json.load(f)
+
+
+@app.route("/api/cargo", methods=["GET"])
+def get_cargo():
+    # Check header
+    override = request.headers.get("X-System-Override")
+
+    if override == "true":
+        return Response("System override denied.", status="418 I'm a teapot", mimetype="text/plain")
+
+    return jsonify(cargo_data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
