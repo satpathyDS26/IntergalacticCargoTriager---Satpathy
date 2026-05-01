8a21f8f0ba3daaf17197fa775d3ca761b6e1ef07
satpathyDS26 <satpathyiasri@gmail.com>
2026-05-02 04:35:38 +0530

    frontend dashboard with cargo

--- Task_2_api.py
+++ Task_2_api.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request, Response
 import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Load JSON data once
 with open("Task 1 - Satpathy - Parser.json", "r") as f:
